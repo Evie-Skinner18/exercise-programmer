2feef703cca8e3c35f018589f94875c3610d71cb
Evie Skinner <evie.skinner@madetech.com>
2022-04-13 10:19:32 +0100

    Made basic React app and added TailwindCSS

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Switch, Route, Link } from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-red-700 underline">Hi Tailwind!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
