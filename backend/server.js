5420656314c28d3e2d69375d0d2c961c0eaeb118
Evie Skinner <evie.skinner@madetech.com>
2022-04-19 18:47:44 +0100

    Changed CORS setting so it will depend on the value of an env var. Submit new exercise functionality works

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import exercises from "./api/exercises.route.js";
 
+dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+const corsOptions = {
+    origin: process.env.REACT_APP_ORIGIN,
+    optionsSuccessStatus: 200
+  }
+
+app.use(cors(corsOptions));
 
 app.use("/api/exercises", exercises);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
